140e3ffa30d2cef10d130aebf227d94f7e59578c
Tran Ngoc Lam Vy <lamvytran1410@gmail.com>
2024-05-27 00:28:45 +0700

    Change Reading Test Stucture

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import NavBar from "@/components/Navbar/Navbar";
 import Reveal from "@/components/Reveal/Reveal";
 
 export default function Home() {
 	return (
-		<div className="flex min-h-screen w-full flex-col items-center justify-between p-24">
-			<Reveal direction="top2bot">
-				<div className="w-40 h-40 rounded-lg bg-black"></div>
-			</Reveal>
-		</div>
+		<>
+			<NavBar />
+			<div className="flex min-h-screen w-full flex-col items-center justify-between p-24 mt-14">
+				<Reveal direction="top2bot">
+					<div className="w-40 h-40 rounded-lg bg-black"></div>
+				</Reveal>
+			</div>
+		</>
 	);
 }
